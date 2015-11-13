17ef4e46500c37e1d1bb761a30d65cd1457d729f
devmkr <mar.roz@wp.pl>
2015-11-13 21:56:57 +0100

    second commit

--- MinesWeeper/Model/Board.cs
+++ MinesWeeper/Model/Board.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 
 namespace MinesWeeper.Model
 {
-    class Board
+
+    class MinnerBoard
     {
+        public Dictionary<FieldAdd, Field> Board { get; private set; }
+
+        public MinnerBoard(int _maxX, int _maxY, int _minesNbr)
+        {
+            Board = new Dictionary<FieldAdd, Field>();
+
+            if (_maxX == 0 || _maxY == 0 || _minesNbr > _maxX * _maxY)
+                throw new ArgumentException();
+
+            //Initize the game board
+            for(int x=0; x < _maxX; x++)
+                for (int y=0; y < _maxY; y++)                
+                    Board.Add(new FieldAdd { X = x, Y = y }, new Field());
+                
+            
+
+        }
+    }
+
+
+
+        
+
+
+
+    class Field
+    {       
+        public FieldOption Option { get; private set; }
+        public int NumberOfNeighbours { get; private set; } = 0;                            
+    }
+
+    internal struct FieldAdd
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+
+
+    enum FieldOption
+    {
+        Safe,
+        Minned,
+        Quessed,
     }
 }
